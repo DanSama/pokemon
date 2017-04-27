0ad3d6dd54668e5174e111f308be182b30eeced5
DanSama <dyhacksource@gmail.com>
2017-04-27 10:15:00 -0500

    Add files via upload

--- project/src/coreFunctions/expPoints.java
+++ project/src/coreFunctions/expPoints.java
@@ -2,9 +2,38 @@ package coreFunctions;
 
 public class expPoints {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	
+	public void setLevel(double PokeLevel) {
+		
+		
+	}
+	
+	public void getLevel(double PokeLevel) {
+		
+		setLevel(PokeLevel);
+		
+	}
+	
+	public void totalEXP(double PokeLevel) {
+		
+		double baseEXP = 100;
+		double expMulti = 2.5 * PokeLevel;
+		double expTotal = PokeLevel + expMulti;	
+		double RequiredExp = expTotal;
+		
+	}
+	
+	public void LevelManager(double PokeLevel, double RequiredExp, double currentEXP) {
+		
+		int baseLevel = 1;
+		int maxLevel = 100;
+		
+		if(currentEXP == RequiredExp){
+			PokeLevel++;
+			
+		}
+		
 	}
 
 }
--- project/src/coreFunctions/pokemon.java
+++ project/src/coreFunctions/pokemon.java
@@ -1,34 +1,10 @@
 package coreFunctions;
 
-import java.util.Scanner();
-
 public class pokemon {
 
-	public static boolean gameplay = false;
-	
-	private static Scanner keyboard = new Scanner(System.in);
-	
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome");
-		System.out.println("1.) Start Game");
-		System.out.println("2.) Load Game");
-		System.out.println("3.) Settings");
-		System.out.println("4.) Quit");
-		int input = keyboard.nextInt();
-		run(input);
-	}
-	
-	public void run(int runCode) {
-		if(runCode == 1) {
-			while(gameplay = true) {
-			
-			
-				gameplay = false;
-			}
-		}
+		// TODO Auto-generated method stub
+
 	}
 
-	
-	
 }
